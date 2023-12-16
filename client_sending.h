5ed2e74c46a6bffbeaa9c0a5a54422c83e7b1115
Rahman <telkomrahman1029@gmail.com>
2023-12-16 14:19:45 +0700

    "sending"

--- client_sending.h
+++ client_sending.h
@@ -1,92 +1,72 @@
 #include <stdio.h>
 #include "client_packages.h"
 
+struct {
+    char name[50];
+	char passp[50];
+	char type[50];
+	int balance;
+	int frequency;
+}sending;
+
+struct {
+    int amount1, amount2, amount3, amount4, amount5, amount6, amount7;
+    float  amount8;
+}amount;
+int sendingUserLaundry(){
+    FILE * file23;
+     file23 = fopen("DataBaju.bin", "wb");
+        printf("=================== HOUSEHOLD ===================\n");
+        printf("[1]. Sprei/Selimut/Sarung Bantal & Guling: ");
+        scanf("%d", &amount.amount1);
+        printf("[2]. Bed Cover Kecil: ");
+        scanf("%d", &amount.amount2);
+        printf("[3]. Bed Cover Sedang: ");
+        scanf("%d", &amount.amount3);
+        printf("[4]. Bed Cover Besar: ");
+        scanf("%d", &amount.amount4);
+        printf("==================== CLOTHES ====================\n");
+        printf("[5]. White Clothes: ");
+        scanf("%d", &amount.amount5);
+        printf("[6]. Suit: ");
+        scanf("%d", &amount.amount6);
+        printf("[7]. Shoes: ");
+        scanf("%d", &amount.amount7);
+        printf("[8]. Wash Kilos: ");
+        scanf("%f", &amount.amount8);
+        fwrite(&amount, sizeof(amount), 1, file23);
+        fclose(file23);
+}
 int clientSending() {
-    int  option, num, amount1, amount2, amount3, total2, total3, total4;
-    float total1, weight;
-    printf("1) Regular Clothes\n2) White Clothes\n3) Rug\n4) Shoes\n5) Exit\n");
-    printf("Choose what you want to laundry: ");
+    int num, option;
+
+ 
+    printf("1) Sending Laundry\n2) Exit\n");
+    printf("Choose what you want: ");
     scanf("%i", &option);
 
     switch(option) {
         case 1:
-        printf("Enter the weight of your clothes(kg): ");
-        scanf("%f", &weight);
-        total1 = weight * 8000;
-        printf("Price for your Regular Clothes laundry is Rp.%f\n", total1);
-        printf("Do you want to\n1) Packages and Payment\n2) Exit\n");
-        printf("Choose what you want next: ");
-        scanf("%i", &num);
-        if (num == 1) {
-            clientPackages();
-        }
-        if (num == 2) {
-            //Client_Homepage();
-        } else {
-            printf("Invalid choice, Please select a valid option!\n");
-        }
+        sendingUserLaundry();
         break;
 
         case 2:
-        printf("Enter how many White Clothes you want to laundry: ");
-        scanf("%i", &amount1);
-        total2 = amount1 * 10000;
-        printf("Price for your White Clothes laundry is Rp.%f\n", total2);
-        printf("Do you want to\n1) Packages and Payment\n2) Exit\n");
-        printf("Choose what you want next: ");
-        scanf("%i", &num);
-        if (num == 1) {
-            clientPackages();
-        }
-        if (num == 2) {
-            //Client_Homepage();
-        } else {
-            printf("Invalid choice, Please select a valid option!\n");
-        }
-        break;
-
-        case 3:
-        printf("Enter how many Rug you want to laundry: ");
-        scanf("%i", &amount2);
-        total3 = amount2 * 10000;
-        printf("Price for your rug laundry is Rp.%i\n", total3);
-        printf("Do you want to\n1) Packages and Payment\n2) Exit\n");
-        printf("Choose what you want next: ");
-        scanf("%i", &num);
-        if (num == 1) {
-            clientPackages();
-        }
-        if (num == 2) {
-            //Client_Homepage();
-        } else {
-            printf("Invalid choice, Please select a valid option!\n");
-        }
-        break;
-
-        case 4:
-        printf("Enter how many shoes you want to laundry: ");
-        scanf("%i", &amount3);
-        total4 = amount3 * 10000;
-        printf("Price for your shoes laundry is Rp.%i\n", total4);
-        printf("Do you want to\n1) Packages and Payment\n2) Exit\n");
-        printf("Choose what you want next: ");
-        scanf("%i", &num);
-        if (num == 1) {
-            clientPackages();
-        }
-        if (num == 2) {
-            //Client_Homepage();
-        } else {
-            printf("Invalid choice, Please select a valid option!\n");
-        }
-        break;
-
-        case 5:
-        printf("Hooh\n");
+        //Client_Homepage();
 
         default:
+       printf("Invalid choice, Please select a valid option!\n");
+        
+    }
+    printf("Do you want to\n1) Packages and Payment\n2) Exit\n");
+    printf("Choose what you want next: ");
+    scanf("%i", &num);
+    if (num == 1) {
+        clientPackages();
+    }
+    else if (num == 2) {
+        //Client_Homepage();
+    } else {
         printf("Invalid choice, Please select a valid option!\n");
-        clientSending();
     }
 
 }
